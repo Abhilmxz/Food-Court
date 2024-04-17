c8d0d09aad21c17f2af6a32e88fd50e9c7c36240
Abhishek <abhishekonline45@gmail.com>
2024-04-17 12:30:41 +0530

    first commit

--- app/_components/CategoryList.jsx
+++ app/_components/CategoryList.jsx
@@ -1,5 +1,6 @@
 "use client"
-import React,{useEffect} from 'react';
+import Image from 'next/image';
+import React,{useEffect,useState} from 'react';
 import GlobalApi from '../_utils/GlobalApi';
 
 function CategoryList () {
@@ -19,10 +20,13 @@ function CategoryList () {
 }
   return (
     <div>
-      <div>
+      <div className='flex gap-4'>
         {categoryList&&categoryList.map((category,index)=>(
-          <div key={index}>
-            <Image src={category.icon?.url} alt={category.name} width={40} height={40}/>
+          <div key={index} 
+          className='flex flex-col items-center gap-2 border p-3 rounded-xl min-w-28'>
+            <Image src={category.icon?.url} alt={category.name} width={40} height={40}
+            />
+            <h2>{category.name}</h2>
           </div>
         ))}
       </div>
