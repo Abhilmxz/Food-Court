05391d15468fbf057d6439a5f3014245fe0a0a85
Abhishek <abhishekonline45@gmail.com>
2024-04-19 15:36:08 +0530

    first commit

---
+++ app/_components/Cart.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Image from 'next/image'
+import { X } from 'lucide-react'
+import {Button} from '@/components/ui/button'
+
+function Cart({ cart }) {
+// fuction of cart total amount calculation function
+    const CalculateCartAmount=()=>{
+        let total=0;
+        cart.forEach((item)=>{
+            total=total+item.price;
+        })
+        return total.toFixed(2);
+    }
+
+  return (
+    <div>
+      <h1 className='text-lg font-bold'>{cart[0]?.restaurant?.name}</h1>
+      <div className='mt-5 flex flex-col gap-3'>
+        <h2 className='font-bold'>My Order</h2>
+        {cart&&cart.map((item, index) => (
+            <div key={index} className='flex justify-between gap-8 items-center'>
+                <div className='flex items-center gap-2'>
+                <Image src={item.productImage} 
+                alt={item.productName} width={40} height={40}
+                className='h-[40px] w-[40px] rounded-lg object-cover'/>
+                <h2 className='font-sm'>{item.productName}</h2>
+                </div>
+                <h2 className='font-bold flex gap-2'>${item.price}</h2>
+                <X className='h-4 w-4 text-red-500'/>
+           </div>
+        ))}
+        {/* adding checkout button and total amount */}
+        <Button>Checkout ${CalculateCartAmount()}</Button>
+      </div>
+    </div>
+  )
+}
+
+export default Cart;
--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -9,6 +9,14 @@ import { UserButton } from '@clerk/nextjs';
 import { useUser } from '@clerk/nextjs';
 import { CartUpdateContext } from '@/app/_context/CartUpdateContext';
 import GlobalApi from '../_utils/GlobalApi';
+import Link from 'next/link';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+import Cart from './Cart';
+
 
 
 function Header() {
@@ -49,10 +57,17 @@ const GetUserCart=()=>{
       {isSignedIn?
       // adding cart and user button
       <div className='flex gap-3 items-center'>
-        <div className='flex gap-2 items-center'>
+        
+      {/* Adding display to card option */}
+      <Popover>
+      <PopoverTrigger asChild><div className='flex gap-2 items-center cursor-pointer'>
       <ShoppingCart/>
       <label className='p-1 px-3 rounded-full bg-slate-200'>{cart?.length}</label>
-      </div>
+      </div></PopoverTrigger>
+      <PopoverContent className="w-full"><Cart cart={cart}/></PopoverContent>
+      </Popover>
+
+      
       <UserButton/>
       </div>
       :<div className='flex gap-5'>
